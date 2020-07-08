d3d99d56dd3a925b67b794151d5ab8201f3fe784
bayeswang <827130441@qq.com>
2020-07-08 09:09:06 +0800

    feat: rollup build.

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import babel from '@rollup/plugin-babel';
+import pkg from "./package.json";
+
+export default {
+  input: "src/index.js",
+  output: [
+    {
+      file: pkg.main,
+      format: "cjs",
+    },
+    {
+      file: pkg.module,
+      format: "es",
+    },
+  ],
+  plugins: [
+    babel({
+      babelHelpers: 'runtime',
+      presets: ["@babel/preset-react"],
+    }),
+  ],
+};
